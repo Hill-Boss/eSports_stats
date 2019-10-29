5fcf84467b37049f59605e6808f64efe8a355b58
Hill-Boss <bosscodycollege@yahoo.com>
2019-10-28 18:20:03 -0600

    file management starting 10.28.19
    6:30

--- eSports_stats/eSports/views.py
+++ eSports_stats/eSports/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def display_home(request):
+    return render(request, 'landingPage/index.html')
+
+def display_home(request):
+    return render(request, 'landingPage/index.html')
--- eSports_stats/eSports_stats/settings.py
+++ eSports_stats/eSports_stats/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+eyguuaxnv*77#$ltq!_&gud^ja_ne-8z=5htgp-x$v))xcyt&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['chb.cs.esports.umt.edu']
 
 
 # Application definition
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'eSports',
+    'eSports',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'eSports_stats.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR, '../eSports/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,8 +118,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    'static'
-
-]
+STATIC_URL = os.path.join(BASE_DIR, '../eSports/static/')
