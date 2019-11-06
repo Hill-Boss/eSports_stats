0c6f1c67c77175d565f996119bb13e508111b071
DeepBlue <bosscody337@gmail.com>
2019-11-05 20:47:27 -0700

    11.05.19
    django admin page setup

--- eSports_stats/eSports/admin.py
+++ eSports_stats/eSports/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+import eSports.models as models
 
 # Register your models here.
+admin.site.register(models.User)
+admin.site.register(models.Role)
+admin.site.register(models.Staff)
+admin.site.register(models.Status)
+admin.site.register(models.Player)
+admin.site.register(models.Game)
+admin.site.register(models.Team_Rank)
+admin.site.register(models.Team)
+admin.site.register(models.user_team)
+admin.site.register(models.Stats)
+admin.site.register(models.user_game)
