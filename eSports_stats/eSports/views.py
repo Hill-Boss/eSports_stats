639651ff3e1475c185f41439a149d5ba12034a93
Hill-Boss <bosscodycollege@yahoo.com>
2019-11-09 17:24:31 -0700

    Update 11.09.19 5:30pm
    added jQuery.js, get data from db on localhost/games_db

--- eSports_stats/eSports/views.py
+++ eSports_stats/eSports/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth import logout
+from .models import User
+from .models import Role
+from .models import Staff
+from .models import Status
+from .models import Player
 from .models import Game
+from .models import Team_Rank
+from .models import Team
+from .models import user_team
+from .models import Stats
+from .models import user_game
 
 # Create your views here.
 def display_home(request):
@@ -13,11 +23,40 @@ def display_dash(request):
     return render(request, 'dashboard/index.html')
 
 def display_test(request):
-    return render(request, 'test/index.html')
+    user_list = User.objects.all()
+    staff_list = Staff.objects.all()
+    player_list = Player.objects.all()
+    games_list = Game.objects.all()
+    Team_list = Team.objects.all()
+    user_team_list = user_team.objects.all()
+    user_game_list = user_game.objects.all()
+    return render(request, 'test/index.html', {
+        'Users': user_list,
+        'Staff': staff_list,
+        'Players': player_list,
+        'Games': games_list,
+        'Teams': Team_list,
+        'user_team': user_team_list,
+        'user_game': user_game_list
+        })
 
 def games_db(request):
+    user_list = User.objects.all()
+    staff_list = Staff.objects.all()
+    player_list = Player.objects.all()
     games_list = Game.objects.all()
-    return render(request, 'test/games_db.html', {'Game': games_list})
+    Team_list = Team.objects.all()
+    user_team_list = user_team.objects.all()
+    user_game_list = user_game.objects.all()
+    return render(request, 'test/games_db.html', {
+        'Users': user_list,
+        'Staff': staff_list,
+        'Players': player_list,
+        'Games': games_list,
+        'Teams': Team_list,
+        'user_team': user_team_list,
+        'user_game': user_game_list
+        })
 
 def display_login(request):
     return render(request, 'accounts/login/login.html')
