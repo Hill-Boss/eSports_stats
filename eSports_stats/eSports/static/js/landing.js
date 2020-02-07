ab18defbec365ea69d079157c41f71d1047e58c6
Kody <kodyneill@gmail.com>
2020-02-07 11:00:53 -0700

    01/07/2020

--- eSports_stats/eSports/static/js/landing.js
+++ eSports_stats/eSports/static/js/landing.js
@@ -10,3 +10,22 @@ function signUp() {
   pswd = document.getElementById('signUp_pswd').value;
   pswdR = document.getElementById('signUp_pswd_R').value;
 }
+
+function showDrop() {
+  document.getElementById('myDropdown').classList.toggle("show");
+}
+
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
