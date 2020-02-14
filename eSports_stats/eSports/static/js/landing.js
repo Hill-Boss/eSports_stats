241d7816826845805129135509cc0deec36cc405
Kody <kodyneill@gmail.com>
2020-02-14 10:44:33 -0700

    2/14/2020

--- eSports_stats/eSports/static/js/landing.js
+++ eSports_stats/eSports/static/js/landing.js
@@ -5,7 +5,7 @@ function showDrop() {
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+  if (!event.target.matches('.drop')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
@@ -14,5 +14,37 @@ window.onclick = function(event) {
         openDropdown.classList.remove('show');
       }
     }
+  }
+}
+var Players = {
+  Apex: [
+    "VaccaFlacca",
+    "JayX",
+    "Blitz"
+  ],
+  LeagueOfLegends: [
+    "Vegikarp",
+    "Wish",
+    "Aimbot.exe"
+  ],
+  RocketLeague: [
+    "KogtreyFu",
+    "Soul",
+    "VaccaFlacca"
+  ]
+}
+
+function dropPlayers(Game){
+
+  var div = document.getElementById(Game);
+
+  for (var i = 0; i < Players[Game].length; i++) {
+    Player = Players[Game][i];
+    var button = document.createElement("Button");
+    button.style.width = '100%';
+    button.classList.add("player-button");
+    var playerName = document.createTextNode(Player);
+    button.appendChild(playerName);
+    div.appendChild(button);
   }
 }
